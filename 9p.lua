ddbdc082df0248f78e4e608b8409e419ce0ad2d0
Iruatã Souza <iru.muzgo@gmail.com>
2014-07-25 18:49:03 -0300

    Change putstr to return the number of bytes inserted in the message.

--- 9p.lua
+++ 9p.lua
@@ -40,12 +40,13 @@ function num9p(offset, size)
 end
 
 function putstr(to, s)
-  if #s > #to - 2 then return end
+  if #s > #to - 2 then return 0 end
 
   local p = to:segment():layout{len = num9p(0, 2), s = {2, #s, 's'}}
 
   p.len = #s
   p.s = s
+  return 2 + #s
 end
 
 function getstr(from)
@@ -113,17 +114,18 @@ function attach(uname, aname)
 
   tx.fid  = newfid()
   tx.afid = -1
-  putstr(tx:segment(15), uname)
-  putstr(tx:segment(15 + 2 + #uname), aname)
+  local n = putstr(tx:segment(15), uname)
+  n = n + putstr(tx:segment(15 + n), aname)
   
-  local size = putheader(tx, 104, 8 + 2 + #uname + 2 + #aname)
-  dio.write(tx, 0, size)
+  n = putheader(tx, 104, 8 + n)
+  dio.write(tx, 0, n)
 
   local rx = rxbuf:segment()
   rx:layout(LRattach)
   readmsg(rx)
 end
 
+-- name == nil clones ofid to nfid
 function walk(ofid, nfid, name)
   local LTwalk = data.layout{
                  fid    = num9p(7, 4),
@@ -140,22 +142,23 @@ function walk(ofid, nfid, name)
 
   local tx = txbuf:segment()
   tx:layout(LTwalk)
-
-  -- #name == 0 clones the fid 
-  local size = putheader(tx, 110, 10 + (#name ~= 0 and 2 or 0) + #name)
   tx.fid    = ofid
   tx.nfid   = nfid
-  tx.nwname = (#name ~= 0 and 1 or 0) 
-  putstr(tx:segment(17), name)
-  dio.write(tx, 0, size)
+
+  local n = 0
+  if (name) then
+    tx.nwname = 1
+    n = putstr(tx:segment(17), name)
+  else
+    tx.nwname = 0
+  end
+
+  n = putheader(tx, 110, 10 + n)
+  dio.write(tx, 0, n)
 
   local rx = rxbuf:segment()
   rx:layout(LRwalk)
   readmsg(rx)
-end
-
-function fidclone(ofid, nfid)
-  walk(ofid, nfid, "")
 end
 
 function _test()
@@ -165,7 +168,7 @@ function _test()
    attach("iru", "")
    local f = newfid() 
    walk(0, f, "/tmp")
-   fidclone(f, newfid())
+   walk(f, newfid())
 end
 
 _test()
